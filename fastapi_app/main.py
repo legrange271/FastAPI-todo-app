771a3d81313252e44864846a5b95f52c4587f899
Henry Ling <henrytling@hotmail.co.uk>
2024-03-19 12:32:25 +0000

    Updated main.py to include better Response handling

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -1,26 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 from models import Item, items
 
 app = FastAPI()
 
-@app.get("/")
-async def home():
-    """Home function"""
+@app.get("/", status_code=200)
+def home():
+    """Home function for basic functoinality"""
     return {"message": "Hello world!"}
 
-@app.get("/items")
+@app.get("/items", status_code=200)
 def return_items():
     return {"Items": items}
 
-@app.get("/items/{item_id}")
-def get_item(item_id:int):
+@app.get("/items/{item_id}", status_code=200)
+def get_item(item_id:int, response:Response):
     if item_id in items:
         return items[item_id]
     else:
+        response.status_code = status.HTTP_404_NOT_FOUND
         return {"message": "id not found"}
 
-@app.put("/items/{item_id}")
-def add_item(item_id:int, item: Item):
+@app.post("/items/{item_id}", status_code=201)
+def add_item(item_id:int, item: Item, response:Response):
     """End point for adding a new todo item"""
     items[item_id] = item
     return {"Todo":item.name, "Id": item_id}
