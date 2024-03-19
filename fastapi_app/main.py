f95d5ac48898e2d6139b7e7f94d6c04363cdf2bc
Henry Ling <henrytling@hotmail.co.uk>
2024-03-19 12:53:21 +0000

    added functionality for when you try to create an item that already exists

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -5,15 +5,17 @@ app = FastAPI()
 
 @app.get("/", status_code=200)
 def home():
-    """Home function for basic functoinality"""
+    """Specify basic app functionality"""
     return {"message": "Hello world!"}
 
 @app.get("/items", status_code=200)
-def return_items():
+def get_items():
+    """Endpoint for listing all items"""
     return {"Items": items}
 
 @app.get("/items/{item_id}", status_code=200)
 def get_item(item_id:int, response:Response):
+    """Endpoint for getting an item by id"""
     if item_id in items:
         return items[item_id]
     else:
@@ -23,6 +25,10 @@ def get_item(item_id:int, response:Response):
 @app.post("/items/{item_id}", status_code=201)
 def add_item(item_id:int, item: Item, response:Response):
     """End point for adding a new todo item"""
-    items[item_id] = item
-    return {"Todo":item.name, "Id": item_id}
+    if item_id in items:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return {"message": "Item already exists"}
+    else:
+        items[item_id] = item
+        return {"Todo":item.name, "Id": item_id}
     
