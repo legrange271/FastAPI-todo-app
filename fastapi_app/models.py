fdc8f8082e6621d996d1cb61498b54be65519966
Henry Ling <henrytling@hotmail.co.uk>
2024-03-19 12:31:52 +0000

    updated the Items to use optional instead of Union of bool and None for better readability

--- fastapi_app/models.py
+++ fastapi_app/models.py
@@ -1,8 +1,10 @@
 from pydantic import BaseModel
-from typing import Union
+from typing import Optional
 
 class Item(BaseModel):
     name: str
-    checked: Union[bool, None] = None
+    checked: Optional[bool] = None
 
+# Here we define a simple in memory non persistent file store 
+# TODO: add a start up and end command to save this and keep it secure
 items = {}
