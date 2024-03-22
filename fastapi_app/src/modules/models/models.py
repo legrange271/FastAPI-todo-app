c602431485200edb8e65782de3684882d5740012
Henry Ling <henrytling@hotmail.co.uk>
2024-03-22 11:55:44 +0000

    Updated models so that it defaults to false instead of none

--- fastapi_app/src/modules/models/models.py
+++ fastapi_app/src/modules/models/models.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 class Item(BaseModel):
     name: str
-    checked: Optional[bool] = None
+    checked: Optional[bool] = False 
 
 # Here we define a simple in memory non persistent file store 
 # TODO: add a start up and end command to save this and keep it secure
